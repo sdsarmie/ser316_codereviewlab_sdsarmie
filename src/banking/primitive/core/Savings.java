febc729ba9b2db28d227cb8e75942c1ea475484d
sdsarmie <sadai.sarmiento@gmail.com>
2017-04-10 22:51:42 -0700

    replace account type literals with constants (bug #8)

--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -1,8 +1,10 @@
 package banking.primitive.core;
 
 public class Savings extends Account {
+
 	private static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
+	private static final String ACCOUNT_TYPE = "Savings";
+	private int numWithdraws = 0;	
 
 	public Savings(String name) {
 		super(name);
@@ -44,9 +46,9 @@ public class Savings extends Account {
 		return false;
 	}
 	
-	public String getType() { return "Checking"; }
-
+	public String getType() { return ACCOUNT_TYPE; }
+	
 	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
+		return ACCOUNT_TYPE + ": " + getName() + ": " + getBalance();
 	}
 }
